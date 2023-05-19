bb8ccc1818cb467c264fad5d6b14511037d74249
sha-shrestha <1738675+sha-shrestha@users.noreply.github.com>
2023-05-19 09:04:03 +0100

    Create sfn2png.py

---
+++ sfn2png.py
@@ -0,0 +1,44 @@
+import pygraphviz as pgv
+import json
+
+def render_state(state_name, state_data, graph):
+    if state_data['Type'] == 'Parallel':
+        graph.add_node(state_name, label=state_name, shape='box', style='filled', fillcolor='lightblue')
+
+        # Render parallel branches
+        for branch in state_data['Branches']:
+            branch_start = branch['StartAt']
+            branch_states = branch['States']
+            render_state(branch_start, branch_states[branch_start], graph)
+            graph.add_edge(state_name, branch_start)
+    else:
+        graph.add_node(state_name, label=state_name, shape='box')
+        
+        if 'Next' in state_data:
+            next_state = state_data['Next']
+            graph.add_edge(state_name, next_state)
+
+
+def render_step_function_json(json_path, output_path):
+    # Load JSON file
+    with open(json_path, 'r') as f:
+        json_data = json.load(f)
+
+    # Create directed graph
+    graph = pgv.AGraph(directed=True)
+
+    # Add states and transitions to the graph
+    for state_name, state_data in json_data['States'].items():
+        render_state(state_name, state_data, graph)
+
+    # Render graph to PNG
+    graph.draw(output_path, prog='dot', format='png')
+
+    print(f"Step Function JSON rendered as PNG: {output_path}")
+
+
+# Usage example
+json_path = 'path/to/your/step_function.json'
+output_path = 'path/to/save/output.png'
+
+render_step_function_json(json_path, output_path)
